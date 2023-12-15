e1d852410861134c4b78a4189d3cd81ad692a63a
Dmytro Fal <dmitry.fal@protonmail.com>
2023-12-15 17:34:10 +0200

    Move vim options to separate file. Add line numbers

---
+++ lua/vim-options.lua
@@ -0,0 +1,14 @@
+vim.g.mapleader = " "
+vim.cmd("set expandtab")
+vim.cmd("set tabstop=2")
+vim.cmd("set softtabstop=2")
+vim.cmd("set shiftwidth=2")
+vim.cmd("set number")
+vim.cmd([[
+  augroup numbertoggle
+    autocmd!
+    autocmd BufEnter,FocusGained,InsertLeave * set relativenumber
+    autocmd BufLeave,FocusLost,InsertEnter * set norelativenumber
+  augroup END
+]])
+

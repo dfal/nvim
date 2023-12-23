44029f9667284be67088baacf2a3ad5ebe6c32c4
Dmytro Fal <dmitry.fal@protonmail.com>
2023-12-23 02:33:31 +0200

    Add lsp-config

---
+++ lua/plugins/lsp-config.lua
@@ -0,0 +1,35 @@
+return {
+  {
+    "williamboman/mason.nvim",
+    config = function()
+      require("mason").setup()
+    end
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    config = function()
+      require("mason-lspconfig").setup({
+        ensure_installed = {
+          "lua_ls",
+          "tsserver",
+          "gopls"
+        }
+      })
+    end
+  },
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      local lspconfig = require("lspconfig")
+      lspconfig.lua_ls.setup({})
+      lspconfig.tsserver.setup({})
+      lspconfig.gopls.setup({})
+      vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, {})
+      vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
+      vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+      vim.keymap.set("n", "gi", vim.lsp.buf.implementation, {})
+      vim.keymap.set("n", "gr", require("telescope.builtin").lsp_references, {})
+      vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+    end
+  }
+}
--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -2,7 +2,7 @@ return {
   "nvim-lualine/lualine.nvim",
   requires = { "nvim-tree/nvim-web-devicons", opt = true },
   config = function()
-    require('lualine').setup({
+    require("lualine").setup({
       options = {
         theme = "dracula"
       }

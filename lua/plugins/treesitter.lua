2d69d29b9479b413d282ce36d18bf002d9d1e77b
Dmytro Fal <dmitry.fal@protonmail.com>
2023-12-28 04:07:54 +0200

    Add autotag

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,12 +1,13 @@
 return {
   "nvim-treesitter/nvim-treesitter",
+  dependencies = { "windwp/nvim-ts-autotag" },
   build = ":TSUpdate",
   config = function()
-    local config = require("nvim-treesitter.configs")
-    config.setup({
+    require("nvim-treesitter.configs").setup({
       auto_install = true,
       highlight = { enable = true },
-      indent = { enable = true }
+      indent = { enable = true },
+      autotag = { enable = true },
     })
-  end
+  end,
 }
